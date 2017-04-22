d43c92366b4242bff5c2f5c45c1cbc562a52bbb9
MAURICIO VALDES <MAURICIO@iMac-de-MAURICIO.local>
2017-04-21 20:40:38 -0500

    callback response firebase

--- server.js
+++ server.js
@@ -76,22 +76,7 @@ app.post("/contacts", function(req, res) {
       try {
           var speech = 'empty speech';
 
-          if (req.body) {
-              var requestBody = req.body;
-
-              if (requestBody.result) {
-                  speech = '';
-
-                  if (requestBody.result.fulfillment) {
-                      speech += requestBody.result.fulfillment.speech;
-                      speech += ' ';
-                  }
-
-                  if (requestBody.result.action) {
-                      speech += 'action: ' + requestBody.result.action;
-                  }
-              }
-          }
+    
 
           console.log('result: ', speech);
 
